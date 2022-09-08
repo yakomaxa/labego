26f793ad612c8d855f37344611f24f2f0cfaf973
yakomaxa <47655565+yakomaxa@users.noreply.github.com>
2022-09-08 23:48:41 +0900

    added lapsego which divides B-region into two subclass S and P

--- labego.py
+++ labego.py
@@ -1,18 +1,21 @@
 from pymol import stored
 
-def ppos2abegos(ppos):
+def ppos2abegos(ppos,divideB):
     abegos=[]
     for resi in range(0,len(ppos[:,0])):
         phi   = float(ppos[resi, 0])
         psi   = float(ppos[resi, 1])
         omega = float(ppos[resi, 2])
-        abegos.append(dihd2abego(phi,psi,omega))
+        abegos.append(dihd2abego(phi,psi,omega,divideB=divideB))
     if (len(ppos[:,0] == len(abegos))):
         return abegos
     else:
         return None
 
-def dihd2abego(phi=-60.0,psi=-45.0,omega=180.0,cisbin=30.0,A_upper=50.0,A_lower=-75.0,G_upper=100,G_lower=-100.0):
+def dihd2abego(phi=-60.0,psi=-45.0,omega=180.0,
+               cisbin=30.0,A_upper=50.0,A_lower=-75.0,
+               G_upper=100,G_lower=-100.0,
+                P_thre = -90, divideB=False):
     if (omega <= cisbin and omega >= -cisbin):
         return "O"
 
@@ -20,7 +23,13 @@ def dihd2abego(phi=-60.0,psi=-45.0,omega=180.0,cisbin=30.0,A_upper=50.0,A_lower=
         if (A_lower <= psi and psi <= A_upper):
             return "A"
         else:
-            return "B"
+            if (divideB):
+                if ( phi < P_thre):
+                    return "P"
+                else:
+                    return "S"
+            else:
+                return "B"
     else:
         if (G_lower <= psi and psi <= G_upper):
             return "G"
@@ -47,11 +56,11 @@ def getomega(target):
 #    omegas.append(180)
     return omegas
 
-def labegO(target="all"):
+def labegO(target="polymer.protein"):
     objs=cmd.get_object_list(target)
-    #print(objs)
-    for tgt in objs:
-        tgt = tgt + " and " + target
+    print(objs)
+    for tgt_tmp in objs:
+        tgt = tgt_tmp + " and " + target
         phipsi=cmd.phi_psi(tgt)
         keys=list(phipsi)
         ppos=[]
@@ -80,10 +89,10 @@ pymol.cmd.extend("labegO", labegO)
 cmd.auto_arg[0]['labegO'] = cmd.auto_arg[0]['align']
 
 
-def labego(target="all", option=""):
+def labego(target="polymer.protein"):
     objs=cmd.get_object_list(target)
-    for tgt in objs:
-        tgt = tgt + " and " + target
+    for tgt_tmp in objs:
+        tgt = tgt_tmp + " and " + target
         phipsi=cmd.phi_psi(tgt)
         keys=list(phipsi)
         ppos=[]
@@ -94,7 +103,31 @@ def labego(target="all", option=""):
             omega = 180.0 # temporary
             ppos.append([phi,psi,omega])
         ppos=np.array(ppos)
-        myabego = ppos2abegos(ppos)
+        myabego = ppos2abegos(ppos,divideB=False)
+        ii = 0
+        for key in keys:
+            label=str(myabego[ii])
+            # Be careful of label should be double quoted when evaluated
+            qlabel='\"' + label + '\"'
+            cmd.label(tgt +" and index "+str(key[1]),expression=qlabel)
+            ii+=1
+            cmd.set("label_size","30")
+
+def lapsego(target="polymer.protein"):
+    objs=cmd.get_object_list(target)
+    for tgt_tmp in objs:
+        tgt = tgt_tmp + " and " + target
+        phipsi=cmd.phi_psi(tgt)
+        keys=list(phipsi)
+        ppos=[]
+
+        for key in keys:
+            phi = phipsi[key][0]
+            psi = phipsi[key][1]
+            omega = 180.0 # temporary
+            ppos.append([phi,psi,omega])
+        ppos=np.array(ppos)
+        myabego = ppos2abegos(ppos,divideB=True)
         ii = 0
         for key in keys:
             label=str(myabego[ii])
@@ -105,4 +138,6 @@ def labego(target="all", option=""):
             cmd.set("label_size","30")
 
 pymol.cmd.extend("labego", labego)
-cmd.auto_arg[0]['labego'] = cmd.auto_arg[0]['align']
+pymol.cmd.extend("lapsego", lapsego)
+cmd.auto_arg[0]['labego'] = cmd.auto_arg[0]['hide']
+cmd.auto_arg[0]['lapsego'] = cmd.auto_arg[0]['hide']
